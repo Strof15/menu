2413452b60ee42e2e6cf889f2cc035387251a78c
KGyulaAI <71291570+KGyulaAI@users.noreply.github.com>
2022-11-17 23:35:26 +0100

    Add files via upload

---
+++ menu-main/menu-main/vsc nagy kristof/menu.js
@@ -0,0 +1,250 @@
+//Házhozszállítás
+function rendelesGombClick() {
+    var checkBox = document.getElementById("RendelesCheckbox");
+    if (document.getElementById("RendelesIranyitoSzam").value != "" && document.getElementById("RendelesTelepules").value != "" && document.getElementById("RendelesUtca").value != "" && document.getElementById("RendelesHazszam").value != "" && document.getElementById("RendelesEmail").value != "" && document.getElementById("RendelesNev").value != "" && document.getElementById("RendelesIranyitoSzam").value != "" && checkBox.checked == true)
+    {
+        alert("Rendelés sikeres! Küldtünk egy visszaigazoló emailt a " + document.getElementById("RendelesEmail").value + " címre. Jó étvágyat!");
+    }
+}
+
+//Asztalfoglalás
+function foglalasGombClick() {
+    var checkBox = document.getElementById("FoglalasCheckbox");
+    if (document.getElementById("FoglalasVendegekSzama").value != "" && document.getElementById("FoglalasNev").value != "" && document.getElementById("FoglalasEmail").value != "" && document.getElementById("FoglalasDatum").required != "" && document.getElementById("FoglalasIdotartam").value != "" && document.getElementById("FoglalasIdopont").value != "" && document.getElementById("FoglalasFizetesModja").value != "" && checkBox.checked == true)
+    {
+        alert("Foglalás sikeres! Küldtünk egy visszaigazoló emailt a " + document.getElementById("RendelesEmail").value + " címre. Hamarosan találkozunk!");
+    }
+}
+
+//Napi menü focus
+function kivalasztasFocus1() {
+    document.getElementById("1").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur1() {
+    document.getElementById("1").style.color = "white";
+}
+
+function kivalasztasFocus2() {
+    document.getElementById("2").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur2() {
+    document.getElementById("2").style.color = "white";
+}
+
+function kivalasztasFocus3() {
+    document.getElementById("3").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur3() {
+    document.getElementById("3").style.color = "white";
+}
+
+function kivalasztasFocus4() {
+    document.getElementById("4").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur4() {
+    document.getElementById("4").style.color = "white";
+}
+
+function kivalasztasFocus5() {
+    document.getElementById("5").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur5() {
+    document.getElementById("5").style.color = "white";
+}
+
+function kivalasztasFocus6() {
+    document.getElementById("6").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur6() {
+    document.getElementById("6").style.color = "white";
+}
+
+function kivalasztasFocus7() {
+    document.getElementById("7").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur7() {
+    document.getElementById("7").style.color = "white";
+}
+
+function kivalasztasFocus8() {
+    document.getElementById("8").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur8() {
+    document.getElementById("8").style.color = "white";
+}
+
+function kivalasztasFocus9() {
+    document.getElementById("9").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur9() {
+    document.getElementById("9").style.color = "white";
+}
+
+function kivalasztasFocus10() {
+    document.getElementById("10").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur10() {
+    document.getElementById("10").style.color = "white";
+}
+
+function kivalasztasFocus11() {
+    document.getElementById("11").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur11() {
+    document.getElementById("11").style.color = "white";
+}
+
+function kivalasztasFocus12() {
+    document.getElementById("12").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur12() {
+    document.getElementById("12").style.color = "white";
+}
+
+function kivalasztasFocus13() {
+    document.getElementById("13").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur13() {
+    document.getElementById("13").style.color = "white";
+}
+
+function kivalasztasFocus14() {
+    document.getElementById("14").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur14() {
+    document.getElementById("14").style.color = "white";
+}
+
+function kivalasztasFocus15() {
+    document.getElementById("15").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur15() {
+    document.getElementById("15").style.color = "white";
+}
+
+function kivalasztasFocus16() {
+    document.getElementById("16").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur16() {
+    document.getElementById("16").style.color = "white";
+}
+
+function kivalasztasFocus17() {
+    document.getElementById("17").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur17() {
+    document.getElementById("17").style.color = "white";
+}
+
+function kivalasztasFocus18() {
+    document.getElementById("18").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur18() {
+    document.getElementById("18").style.color = "white";
+}
+
+function kivalasztasFocus19() {
+    document.getElementById("19").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur19() {
+    document.getElementById("19").style.color = "white";
+}
+
+function kivalasztasFocus20() {
+    document.getElementById("20").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur20() {
+    document.getElementById("20").style.color = "white";
+}
+
+function kivalasztasFocus21() {
+    document.getElementById("21").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur21() {
+    document.getElementById("21").style.color = "white";
+}
+
+function kivalasztasFocus22() {
+    document.getElementById("22").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur22() {
+    document.getElementById("22").style.color = "white";
+}
+
+function kivalasztasFocus23() {
+    document.getElementById("23").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur23() {
+    document.getElementById("23").style.color = "white";
+}
+
+function kivalasztasFocus24() {
+    document.getElementById("24").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur24() {
+    document.getElementById("24").style.color = "white";
+}
+
+function kivalasztasFocus25() {
+    document.getElementById("25").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur25() {
+    document.getElementById("25").style.color = "white";
+}
+
+function kivalasztasFocus26() {
+    document.getElementById("26").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur26() {
+    document.getElementById("26").style.color = "white";
+}
+
+function kivalasztasFocus27() {
+    document.getElementById("27").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur27() {
+    document.getElementById("27").style.color = "white";
+}
+
+function kivalasztasFocus28() {
+    document.getElementById("28").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur28() {
+    document.getElementById("28").style.color = "white";
+}
+
+function kivalasztasFocus29() {
+    document.getElementById("29").style.color = "rgb(255, 51, 0)";
+}
+
+function kivalasztasBlur29() {
+    document.getElementById("29").style.color = "white";
+}
